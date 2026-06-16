05c6989eb54070bf35fcf89f294b28fe1999d90c
ayushman101 <amansinghayush@gmail.com>
2026-06-16 18:49:25 +0000

    query, value, key and weights matrices

--- tinyst/forward.go
+++ tinyst/forward.go
@@ -1,5 +1,7 @@
 package tinyst
 
+import "fmt"
+
 // TODO: need to process input in max seq len batches
 func (m *Model) Forward (input []int) ([][]float64, error) { // input is Id vector , returns logits
 	// 1. create our token embedding matrix
@@ -29,6 +31,48 @@ func (m *Model) Forward (input []int) ([][]float64, error) { // input is Id vect
 		for i := range input_embed {
 			for j := range input_embed[i] {
 				input_embed[i][j] += m.PosEmbed[i][j]
+			}
+		}
+
+		// process Tblocks (transformer layers)
+		for _, t := range m.TBlocks {
+			// transformer attention heads
+			for _, ah:= range t.Attention.Heads {
+				// Query matrix
+				Q, err := Mult (input_embed, ah.W_Q)
+				if err != nil {
+					return nil, err
+				}
+
+				fmt.Println ("Query matrix dimensions ", len (Q), " ", len (Q[0]))
+
+				// Key matrix
+				K, err := Mult (input_embed, ah.W_K)
+				if err != nil {
+					return nil, err
+				}
+
+				fmt.Println ("Key matrix dimensions ", len (K), " ", len (K[0]))
+
+				// Value Matrix
+				V, err := Mult (input_embed, ah.W_V)
+				if err != nil {
+					return nil, err
+				}
+
+				fmt.Println ("Value matrix dimensions ", len (V), " ", len (V[0]))
+
+				// transpose the keys matrix
+				Kt := transpose (K)
+
+				// multiply query with key to get Weights
+				W, err := Mult (Q, Kt)
+				if err != nil {
+					return nil, err
+				}
+
+
+				fmt.Println ("Weights matrix dimensions ", len (W), " ", len (W[0]))
 			}
 		}
 
